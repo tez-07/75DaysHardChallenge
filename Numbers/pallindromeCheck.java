a777133d9a9b94fba36cbb18f3752e65db5d0763
Tez Borgohain <tezborgohain1@gmail.com>
2025-09-19 14:08:45 -0400

    Updated files

--- Numbers/pallindromeCheck.java
+++ Numbers/pallindromeCheck.java
@@ -5,21 +5,41 @@ public class pallindromeCheck {
  
     public static void main(String[] args) {
         
+        /*
         String str = "HellolleH";
         String revString= "";
-            
 
         for(int i=str.length()-1; i>=0; i--){
             char ch = str.charAt(i);
             revString += ch;
         }
 
-
         if(str.equals(revString)){
             System.out.println("The string is pallindrome:"+revString);
         }else{
             System.out.println("The string is not a pallindrome");
         }
+
+        */
+
+        //Approach : Integer is converted to string and compared.
+        int num1 = 123211;
+        String strOrg = String.valueOf(num1);
+        int temp = num1;
+
+        String revStr = "";
+        while (temp!=0) {
+            int digit = temp % 10;
+            revStr = revStr + digit;
+            temp = temp/10;
+        }
+        if(strOrg.equals(revStr)){
+            System.out.println("palindrome");
+        }else{
+            System.out.println("not palindrome");
+        }
+
+
     }
 }
 
