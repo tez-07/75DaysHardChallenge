2a5bce32fc622c076dd103422221f2b0b1dbb19f
Tez Borgohain <tezborgohain1@gmail.com>
2025-10-03 22:24:01 -0400

    new recursion files added

--- Recursion/binarySearch.java
+++ Recursion/binarySearch.java
@@ -3,7 +3,7 @@ public class binarySearch {
     public static void main(String[] args) {
         
         int arr[] = {3,5,2,6,3,6,8,4,9};
-        int target = 6;
+        int target = 2;
         
         int result = binary(arr, target, 0, arr.length);
         System.out.println(result);
@@ -11,7 +11,7 @@ public class binarySearch {
 
     static int binary(int arr[], int target, int start, int end){
 
-        if(start>=end){
+        if(start>=end){         //if the element is not found
             return -1;
         }
         int mid = start + (end-start)/2;
@@ -19,10 +19,10 @@ public class binarySearch {
         if(target==arr[mid]){
             return mid;
         }
-        else if(target<=arr[mid]){
+        else if(target<=arr[mid]){          //end is changing to mid
             return binary(arr, target, 0, mid);
         }
-        else if(target>=arr[mid]){
+        else if(target>=arr[mid]){          //start is changing to mid+1
             return binary(arr, target, mid+1, end);
         }
 
