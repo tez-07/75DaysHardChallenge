2eb5377b23078269944aa64e374ba7ff19f96264
Tez Borgohain <tezborgohain1@gmail.com>
2025-10-28 06:23:27 -0400

    latest

--- LEETCODE/FirstOccur.java
+++ LEETCODE/FirstOccur.java
@@ -3,6 +3,7 @@ package LEETCODE;
 public class FirstOccur {
     public int strStr(String haystack, String needle) {
 
+        /*
         int length = haystack.length(); 
         for(int i=0; i<length; i++){
             if(haystack.contains(needle)){
@@ -10,7 +11,19 @@ public class FirstOccur {
                 return haystack.indexOf(needle);
             }
         }
+        return -1;
+         */
 
+
+        int n = haystack.length();
+        int m = needle.length();
+
+        for(int i=0; i<=n-m; i++){
+            if(haystack.substring(i, i+m).equals(needle)){
+                System.out.println(i);
+                return i;
+            }
+        }
         return -1;
     }
 
